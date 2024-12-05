46fcf07d5bc30d1f6f2b13c308f69afcb560fdf0
nikkov199525 <D:\The_Bat_portable_7.4.6_Zorro\Data\Mail>
2024-12-05 13:12:15 +0300

    small fix in homework from 04.12.24

--- task5.2.js
+++ task5.2.js
@@ -3,4 +3,7 @@ let step2 = await step1.text();
 const regex_time = /(\d{1,2}):(\d{2}):(\d{2})/;
 step2 = step2.match(regex_time);
 const finded_time = step2[0];
-console.log(finded_time);
+if (finded_time[1] === ':') {
+    console.log('0'+finded_time);
+}
+else console.log(finded_time);
